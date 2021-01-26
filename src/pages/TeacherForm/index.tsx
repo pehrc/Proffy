875e221190dc2f82c05428d10f6bdf8f806fc266
unknown <correia.pedro@outlook.com>
2021-01-26 15:58:50 -0300

    implementado o componente PageHeader

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import PageHeader from "../../components/PageHeader";
+import "./styles.css";
 
 function TeacherForm() {
-  return <h1>TeacherForm</h1>;
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer dar aulas." />
+    </div>
+  );
 }
 export default TeacherForm;
